a8d9a5f4475122ce215559154a16c1f94c211326
sbegmanov <sbegmanov@gmail.com>
2024-01-07 21:03:15 +0500

    conda branch

---
+++ code/renv_for_project.R
@@ -0,0 +1,22 @@
+library(renv)
+
+# use ?renv for more info. Shortly, the project works with 
+# the packages' versions of that time.
+
+# install environment packages for the project
+renv::init()
+  
+# sample of installing a package
+renv::install("mgcv")
+
+# for package update check within the project
+renv::snapshot()
+
+# check package directory
+.libPaths()
+
+# check for package existence
+find.package()
+
+# after installation a new package for the project, use it within the project
+# and then snapchot() for renv.
